9da63af2f956aa77b6bce1c1bcd965b369c9e5a3
Sumaiya215 <sumaiyacse15>
2025-01-15 20:44:23 -0800

    login and registration done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -13,12 +13,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
 
-//   apiKey: "AIzaSyAa0uQNJb8AvCaHMkg2_sZpmmPFXuIO7y0",
-//   authDomain: "edura-bb64d.firebaseapp.com",
-//   projectId: "edura-bb64d",
-//   storageBucket: "edura-bb64d.firebasestorage.app",
-//   messagingSenderId: "526749369377",
-//   appId: "1:526749369377:web:a66a772026b278ad55cf60"
+
 };
 
 // Initialize Firebase
