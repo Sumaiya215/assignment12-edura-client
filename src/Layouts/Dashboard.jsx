3dca19b3d6be43c08e292a84510b88bb0641846c
Sumaiya215 <sumaiyacse15>
2025-01-19 00:54:02 -0800

    created study sessions

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -2,38 +2,55 @@ import { NavLink, Outlet } from "react-router-dom";
 import useStudent from "../hooks/useStudent";
 import useTutor from "../hooks/useTutor";
 import useAdmin from "../hooks/useAdmin";
-
+import { FaBookmark, FaList, FaUsers } from "react-icons/fa";
+import { TbNotebook } from "react-icons/tb";
+import { FaBook } from "react-icons/fa6";
+import { HiSquare3Stack3D } from "react-icons/hi2";
+import { SiGoogleclassroom } from "react-icons/si";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { HiOutlineViewGrid } from "react-icons/hi";
 
 const Dashboard = () => {
     const [isStudent] = useStudent();
     const [isTutor] = useTutor();
     const [isAdmin] = useAdmin();
     return (
-        <div className="flex">
+        <div className="md:flex">
             {/* dashboard side bar */}
-           <div className="w-64 min-h-screen bg-base-200">
+           <div className="relative min-h-screen bg-base-200">
             <ul className="menu p-4">
                 {
                    isStudent && <>
-                   <li><NavLink to='/dashboard/view-session'>View Booked Session</NavLink></li>
-                   <li><NavLink to='/dashboard/add-note'>Create Note</NavLink></li>
-                   <li><NavLink to='/dashboard/manage-notes'>Manage Personal Notes</NavLink></li>
-                   <li><NavLink to='/dashboard/view-materials'>View Study Materials</NavLink></li>
+                   <li><NavLink to='/dashboard/view-session'>
+                   <FaBookmark /> View Booked Session</NavLink></li>
+                   <li><NavLink to='/dashboard/add-note'>
+                   <TbNotebook /> Create Note</NavLink></li>
+                   <li><NavLink to='/dashboard/manage-notes'>
+                   <FaBook/> Manage Personal Notes</NavLink></li>
+                   <li><NavLink to='/dashboard/view-materials'>
+                    <FaList/> View Study Materials</NavLink></li>
                    </> 
                 }
                 {
                     isTutor && <>
-                    <li><NavLink to='/dashboard/add-session'>Create Study Session</NavLink></li>
-                    <li><NavLink to='/dashboard/my-sessions'>My Sessions</NavLink></li>
-                    <li><NavLink to='/dashboard/add-materials'>Upload Materials</NavLink></li>
-                    <li><NavLink to='/dashboard/my-materials'>View All Materials</NavLink></li>
+                   <li><NavLink to='/dashboard/add-session'>
+                   <HiSquare3Stack3D /> Create Study Session</NavLink></li>
+                    <li><NavLink to='/dashboard/my-sessions'>
+                    <SiGoogleclassroom /> My Sessions</NavLink></li>
+                    <li><NavLink to='/dashboard/add-materials'>
+                    <FaCloudUploadAlt /> Upload Materials</NavLink></li>
+                    <li><NavLink to='/dashboard/my-materials'>
+                    <HiOutlineViewGrid /> View All Materials</NavLink></li>
                     </>
                 }
                 {
                     isAdmin && <>
-                    <li><NavLink to='/dashboard/users'>View All Users</NavLink></li>
-                    <li><NavLink to='/dashboard/all-sessions'>View All Study Session</NavLink></li>
-                    <li><NavLink to='/dashboard/all-materials'>View All Materials</NavLink></li>
+                    <li><NavLink to='/dashboard/users'>
+                   <FaUsers/> View All Users</NavLink></li>
+                    <li><NavLink to='/dashboard/all-sessions'>
+                    <HiOutlineViewGrid /> View All Study Session</NavLink></li>
+                    <li><NavLink to='/dashboard/all-materials'>
+                    <FaList/> View All Materials</NavLink></li>
                     </>
                 }
 
@@ -42,9 +59,11 @@ const Dashboard = () => {
            </div>
 
         {/* dashboard content */}
-           <div className="flex-1 p-8">
+        <div className="flex-1  md:ml:64">
+           <div className="p-6">
                 <Outlet></Outlet>
            </div>
+        </div>
         </div>
     );
 };
--- src/components/pages/Dashboard/Tutor/AddSession.jsx
+++ src/components/pages/Dashboard/Tutor/AddSession.jsx
@@ -1,10 +1,143 @@
 
+import { useForm } from "react-hook-form"
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+import { Helmet } from "react-helmet-async";
 
 const AddSession = () => {
+    const { user } = useAuth()
+    const axiosSecure = useAxiosSecure()
+    const { register, handleSubmit, formState: { errors } ,reset} = useForm()
+    const onSubmit =async(data) => {
+        console.log(data);
+
+        const dataInfo = {
+            sessionTitle: data.sessionTitle,
+            tutorName: user?.displayName,
+            tutorEmail: user?.email,
+            sessionDuration: data.sessionDuration,
+            registrationStartDate: data.registrationStartDate,
+            registrationEndDate: data.registrationEndDate,
+            classStartDate: data.classStartDate,
+            classEndDate: data.classEndDate,
+            registrationFee: parseInt(data.registrationFee),
+            status: 'pending',
+            sessionDescription: data.sessionDescription
+
+        }
+        const res = await axiosSecure.post('/sessions', dataInfo)
+        console.log(res.data)
+        if(res.data.insertedId){
+            reset();
+            toast.success('Session Successfully Created')
+        };
+
+    }
+
     return (
-        <div>
-            Create A Session
+        <>
+        <Helmet>
+            <title>Add Session | Edura</title>
+        </Helmet>
+        <div className="max-w-xl mx-auto shadow-md rounded-lg p-6 mt-20 mb-12 ">
+            <h1 className="text-2xl font bold text-center mb-6">Create Study Session</h1>
+            <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* Session Title */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Session Title</label>
+                    <input
+                        type="text"{...register('sessionTitle')}
+                        placeholder="Enter session title" className="input input-bordered" required />
+                    {errors.sessionTitle && <span className="text-red-600 text-sm">Session Title is required</span>}
+                </div>
+                {/* Tutor Name */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Tutor Name</label>
+                    <input
+                        type="text"{...register('tutorName')}
+                        defaultValue={user?.displayName} className="input input-bordered bg-gray-100" readOnly />
+                </div>
+                {/* Tutor Email */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Tutor Email</label>
+                    <input
+                        type="text"{...register('tutorEmail')}
+                        defaultValue={user?.email} className="input input-bordered bg-gray-100" readOnly />
+                </div>
+                {/* Session Duration */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Session Duration</label>
+                    <input
+                        type="text"{...register('sessionDuration')}
+                        placeholder="duration... " className="input input-bordered" required />
+                    {errors.sessionDuration && <span className="text-red-600 text-sm">Session Duration is required</span>}
+                </div>
+                {/* Registration Start Date */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Registration Start Date</label>
+                    <input
+                        type="date"{...register('registrationStartDate')}
+                        className="input input-bordered" required />
+                    {errors.registrationStartDate && <span className="text-red-600 text-sm">Registration Start Date is required</span>}
+                </div>
+                {/* Registration End Date */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Registration End Date</label>
+                    <input
+                        type="date"{...register('registrationEndDate')}
+                        className="input input-bordered" required />
+                    {errors.registrationEndDate && <span className="text-red-600 text-sm">Registration Start Date is required</span>}
+                </div>
+                {/* Class Start Date */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Class Start Date</label>
+                    <input
+                        type="date"{...register('classStartDate')}
+                        className="input input-bordered" required />
+                    {errors.classStartDate && <span className="text-red-600 text-sm">Class Start Date is required</span>}
+                </div>
+                {/* Class End Date */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Class End Date</label>
+                    <input
+                        type="date"{...register('classEndDate')}
+                        className="input input-bordered" required />
+                    {errors.classEndDate && <span className="text-red-600 text-sm">Class End Date is required</span>}
+                </div>
+                {/* Registration Fee */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Registration Fee</label>
+                    <input
+                        type="number" {...register('registrationFee')}
+                        defaultValue={0} className="input input-bordered bg-gray-100" readOnly />
+                </div>
+                {/* Status */}
+                <div>
+                    <label className="block text-sm font-medium mb-1">Status</label>
+                    <select
+                       {...register('status')}
+                        defaultValue="pending" className="select select-bordered bg-gray-100"  >
+                        <option value="pending">Pending</option>
+                        <option value="approved">Approved</option>
+                        <option value="rejected">Rejected</option>
+                    </select>    
+                </div>
+                {/* Session Description */}
+                <div className="md:col-span-2">
+                    <label className="block text-sm font-medium mb-1">Session Description</label>
+                    <textarea
+                        {...register('sessionDescription')}
+                        placeholder="Write Session Description Here..." className="textarea textarea-bordered w-full" required />
+                    {errors.sessionDescription && <span className="text-red-600 text-sm">Session Description is required</span>}
+                </div>
+                {/* Submit Button */}
+                <div className="md:col-span-2">
+                    <button type="submit" className="btn btn-primary w-full">Submit</button>
+                </div>
+            </form>
         </div>
+        </>
     );
 };
 
